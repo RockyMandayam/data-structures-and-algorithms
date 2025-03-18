3dc6ae3a573368215acaae6e1148ab675fc787bc
Rocky Mandayam <rocky.mandayam@gmail.com>
2025-03-18 10:11:10 -0700

    more cleanup

--- graph.py
+++ graph.py
@@ -16,8 +16,8 @@ class Graph:
     
     Attributes:
         _nodes (Mapping[Hashable, Mapping]): Map from node (a Hashable) to its attributes (a Mapping).
+        _edges
         _neighbors (Mapping[Hashable, set]): Map from node to its neighbors (a set is used for neighbors - no duplicates!)
-        _num_edges (int): number of edges
         _name (str): name of graph
     """
 
@@ -26,9 +26,27 @@ class Graph:
         nodes: Mapping[Hashable, Mapping] | Iterable[Hashable] | int | None = None,
         edges: Mapping[tuple[Hashable, Hashable], Mapping] | Sequence[tuple[Hashable, Hashable]] | None = None,
         name: str | None = None,
-        skip_duplicate_edges: bool = False
+        skip_duplicate_edges: bool = False,
     ) -> None:
-        ### convert nodes to the right format
+        self.name = name or ""
+        self._nodes = self._construct_and_validate_nodes(nodes)
+        self._edges = Graph._construct_and_validate_edges(self._nodes, edges, skip_duplicate_edges)
+
+        # adjacency list representation is easier for some tasks
+        neighbors: Mapping[Hashable, set] = {node: set() for node in self._nodes}
+        for u, v in self._edges:
+            neighbors[u].add(v)
+            neighbors[v].add(u)
+        # NOTE: we are storing redundant info in edges and neighbors,
+        # since each data structure is useful for different tasks
+        # edges contains more info though, since it contains edge attributes
+        self._neighbors = neighbors
+    
+    def _construct_and_validate_nodes(
+        self,
+        nodes: Mapping[Hashable, Mapping] | Iterable[Hashable] | int | None
+    ) -> Mapping[Hashable, Mapping]:
+        """Construct nodes Mapping by converting input nodes to the right format, and validate."""
         # handle None case
         if nodes is None:
             nodes = {}
@@ -51,8 +69,15 @@ class Graph:
         # validate nodes
         if None in nodes:
             raise ValueError("None is not a valid node.")
-        
-        ### convert edges to the right format
+        return nodes
+    
+    @staticmethod
+    def _construct_and_validate_edges(
+        nodes: Mapping[Hashable, Mapping],
+        edges: Mapping[tuple[Hashable, Hashable], Mapping] | Sequence[tuple[Hashable, Hashable]] | None,
+        skip_duplicate_edges: bool
+    ) -> Mapping[tuple[Hashable, Hashable], Mapping]:
+        """Construct edges Mapping by converting input edges to the right format, and validate."""
         # handle None case
         if edges is None:
             edges = {}
@@ -60,6 +85,7 @@ class Graph:
         # use temp 'edges_map' name to not override 'edges' name
         if edges and not isinstance(edges, Mapping):
             edges_map: Mapping[tuple(Hashable, Hashable), Mapping] = {(u,v): {} for (u,v) in edges}
+            # easier way to check for duplicates than checking iteratively while adding edges
             if not skip_duplicate_edges and len(edges_map) != len(edges):
                 raise ValueError("Duplicate edges not allowed")
             edges = edges_map
@@ -67,40 +93,24 @@ class Graph:
         # if edges is already a Mapping, it can have duplicates (by having (u,v) and (v,u) which represent
         # the same edge), and it can have a self-loop. Check for these here. But it can't have "explicit duplicates"
         # in that it can't repeat (u,v) twice
-        cleaned_edges = {}
+        validated_edges = {}
         for u, v in edges:
             if u == v:
                 raise ValueError(f"Found self-loop for node {u}; self-loops are not allowed.")
-            if (v,u) in cleaned_edges:
+            if (v,u) in validated_edges:
                 if skip_duplicate_edges:
                     continue
                 else:
                     raise ValueError(f"Found duplicate edge {(u,v)}; duplicate edges not allowed")
-            cleaned_edges[(u,v)] = edges[(u,v)]
-        edges = cleaned_edges
+            validated_edges[(u,v)] = edges[(u,v)]
+        edges = validated_edges
         for u, v in edges:
             if u not in nodes:
                 raise ValueError(f"Unknown node {u} found in edges.")
             if v not in nodes:
                 raise ValueError(f"Unknown node {v} found in edges.")
-
-        # adjacency list representation is easier for some tasks
-        neighbors: Mapping[Hashable, set] = {node: set() for node in nodes}
-        num_edges = 0
-        neighbors = {node: set() for node in nodes}
-        for u, v in edges:
-            neighbors[u].add(v)
-            neighbors[v].add(u)
-            num_edges += 1
-        
-        self._nodes = nodes
-        self._edges = edges
-        # NOTE: we are storing redundant info in edges and neighbors,
-        # since each data structure is useful for different tasks
-        # edges contains more info though, since it contains edge attributes
-        self._neighbors = neighbors
-        self._num_edges = num_edges
-        self.name = name or ""
+        return edges
+    
     
     def get_node_attribute(self, node: Hashable, key: Any) -> Any:
         # TODO maybe return copy
@@ -114,11 +124,11 @@ class Graph:
         return len(self._nodes)
     
     def num_edges(self) -> int:
-        return self._num_edges
+        return len(self._edges)
     
     def __str__(self) -> str:
         num_nodes = len(self)
-        return f"Graph '{self.name}' with {num_nodes} node{'s' if num_nodes != 1 else ''} and {self._num_edges} edge{'s' if self._num_edges != 1 else ''}"
+        return f"Graph '{self.name}' with {num_nodes} node{'s' if num_nodes != 1 else ''} and {self.num_edges()} edge{'s' if self.num_edges() != 1 else ''}"
     
     def __iter__(self) -> Iterator:
         return iter(self._nodes)
@@ -162,5 +172,3 @@ class Graph:
             raise ValueError(f"Edge ({u}, {v}) already exists.")
         self._edges[(u,v)] = attributes
         self._neighbors[u].add(v)
-        self._num_edges += 1
-
