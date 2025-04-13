88af6b514c239e29d7103c733209501e120fb30a
Rocky Mandayam <rocky.mandayam@gmail.com>
2025-04-13 13:25:41 -0700

    update topological sort to detect directed cycles

--- dsa/graphs/analysis/topological_sort.py
+++ dsa/graphs/analysis/topological_sort.py
@@ -6,8 +6,7 @@ from dsa.graphs.digraph import Digraph
 
 def topological_sort(dg: Digraph) -> list[Hashable]:
     """Topologically sort a digraph; raises error if digraph is cyclic."""
-    _, _, _, postorder, _, is_cyclic, _ = dfs(dg, recursive=True)
-    # TODO need to implement directed is_cyclic
-    # if is_cyclic:
-    #     raise ValueError("Given digraph is cyclic; cannot topologically sort it.")
+    _, _, _, postorder, _, _, directed_is_cyclic = dfs(dg, recursive=True)
+    if directed_is_cyclic:
+        raise ValueError("Given digraph is cyclic; cannot topologically sort it.")
     return postorder[::-1]
--- dsa/graphs/analysis/topological_sort_test.py
+++ dsa/graphs/analysis/topological_sort_test.py
@@ -1,3 +1,5 @@
+import pytest
+
 from dsa.graphs.analysis.topological_sort import topological_sort
 from dsa.graphs.digraph import Digraph
 from dsa.utils import get_key_to_index
@@ -31,3 +33,17 @@ def test_topological_sort() -> None:
     sorted_node_to_index = get_key_to_index(sorted_nodes)
     for u, v in dg.get_edges():
         assert sorted_node_to_index[u] < sorted_node_to_index[v]
+
+    dg.add_edge(("E", "B"))
+    with pytest.raises(ValueError):
+        topological_sort(dg)
+
+    dg.remove_edge(("E", "B"))
+    dg.add_edge(("L", "J"))
+    sorted_nodes = topological_sort(dg)
+    sorted_node_to_index = get_key_to_index(sorted_nodes)
+    for u, v in dg.get_edges():
+        assert sorted_node_to_index[u] < sorted_node_to_index[v]
+    dg.add_edges((("I", "G"), ("G", "I")))
+    with pytest.raises(ValueError):
+        topological_sort(dg)
