225a2db66f1902359719973b67d1eeecf2d1f1d3
Rocky Mandayam <rocky.mandayam@gmail.com>
2025-04-14 18:06:32 -0700

    Add BFS topo sort (Kahn's algorithm)

--- dsa/graphs/analysis/topological_sort.py
+++ dsa/graphs/analysis/topological_sort.py
@@ -1,12 +1,47 @@
 from collections.abc import Hashable
 
 from dsa.graphs.analysis.traversal.dfs import dfs
+from dsa.graphs.analysis.traversal_type import TraversalType
 from dsa.graphs.digraph import Digraph
 
 
-def topological_sort(dg: Digraph) -> list[Hashable]:
-    """Topologically sort a digraph; raises error if digraph is cyclic."""
+def topological_sort(
+    dg: Digraph, traversal_type: TraversalType = TraversalType.DFS
+) -> list[Hashable]:
+    """Topologically sort a digraph; raises error if digraph is cyclic.
+
+    NOTE: only traversal types DFS and BFS are allowed!
+    """
+    if traversal_type == TraversalType.DFS:
+        func = _dfs_topological_sort
+    elif traversal_type == TraversalType.BFS:
+        # can't use the typical BFS algorithm natively (AFIAK) to get the topological order
+        # have to use Kahn's algorithm instead, which is still "BFS" in its node processing order
+        func = _kahn_topological_sort
+    else:
+        raise ValueError(f"{traversal_type=} must be DFS or BFS")
+    return func(dg)
+
+
+def _dfs_topological_sort(dg: Digraph) -> list[Hashable]:
     _, _, _, postorder, _, _, directed_is_cyclic = dfs(dg, recursive=True)
     if directed_is_cyclic:
         raise ValueError("Given digraph is cyclic; cannot topologically sort it.")
     return postorder[::-1]
+
+
+def _kahn_topological_sort(dg: Digraph) -> list[Hashable]:
+    remaining_in_degrees = {u: dg.get_in_degree(u) for u in dg}
+    queue = [u for u, deg in remaining_in_degrees.items() if deg == 0]
+    sorted_nodes = []
+    while queue:
+        u = queue.pop(0)
+        sorted_nodes.append(u)
+        for v in dg[u]:
+            remaining_in_degrees[v] -= 1
+            if remaining_in_degrees[v] == 0:
+                queue.append(v)
+    if len(sorted_nodes) == len(dg):
+        return sorted_nodes
+    else:
+        raise ValueError("Given digraph is cyclic; cannot topologically sort it.")
--- dsa/graphs/analysis/topological_sort_test.py
+++ dsa/graphs/analysis/topological_sort_test.py
@@ -1,11 +1,13 @@
 import pytest
 
 from dsa.graphs.analysis.topological_sort import topological_sort
+from dsa.graphs.analysis.traversal_type import TraversalType
 from dsa.graphs.digraph import Digraph
 from dsa.utils import get_key_to_index
 
 
-def test_topological_sort() -> None:
+@pytest.mark.parametrize("traversal_type", (TraversalType.DFS, TraversalType.BFS))
+def test_topological_sort(traversal_type: TraversalType) -> None:
     dg = Digraph(
         nodes="ABCDEFGHIJKL",
         edges=(
@@ -28,7 +30,7 @@ def test_topological_sort() -> None:
             # ("L", "J"),
         ),
     )
-    sorted_nodes = topological_sort(dg)
+    sorted_nodes = topological_sort(dg, traversal_type=traversal_type)
     # key->index to faster checks
     sorted_node_to_index = get_key_to_index(sorted_nodes)
     for u, v in dg.get_edges():
@@ -36,14 +38,14 @@ def test_topological_sort() -> None:
 
     dg.add_edge(("E", "B"))
     with pytest.raises(ValueError):
-        topological_sort(dg)
+        topological_sort(dg, traversal_type=traversal_type)
 
     dg.remove_edge(("E", "B"))
     dg.add_edge(("L", "J"))
-    sorted_nodes = topological_sort(dg)
+    sorted_nodes = topological_sort(dg, traversal_type=traversal_type)
     sorted_node_to_index = get_key_to_index(sorted_nodes)
     for u, v in dg.get_edges():
         assert sorted_node_to_index[u] < sorted_node_to_index[v]
     dg.add_edges((("I", "G"), ("G", "I")))
     with pytest.raises(ValueError):
-        topological_sort(dg)
+        topological_sort(dg, traversal_type=traversal_type)
